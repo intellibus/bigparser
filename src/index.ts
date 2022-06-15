e8fd7fa7ad9d15a4c96992052f856f80e2cd464b
a-n-u-b-i-s <watalanubhava@gmail.com>
2022-06-15 18:48:19 -0400

    fix: updated build command

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,21 @@
-import { addColumn } from './addColumn';
-import { bulkCrud } from './bulkCrud';
-import { createGrid } from './createGrid';
-import { createTab } from './createTab';
-import { deleteByQuery } from './deleteByQuery';
-import { deleteByRowId } from './deleteByRowId';
-import { deleteTab } from './deleteTab';
-import { deleteGrid } from './deleteGrid';
-import { getGridMetadata } from './getGridMetadata';
-import { getMultisheetMetadata } from './getMultisheetMetadata';
-import { insert } from './insert';
-import { search } from './search';
-import { searchCount } from './searchCount';
-import { searchDistinct } from './searchDistinct';
-import { setupLinkedColumn } from './setupLinkedColumn';
-import { updateByQuery } from './updateByQuery';
-import { updateByRowId } from './updateByRowId';
-import { updateColumnDatatype } from './updateColumnDatatype';
-import { updateColumnDataSource } from './updateColumnDataSource';
-import { updateTab } from './updateTab';
-
-export {
-  addColumn,
-  bulkCrud,
-  createGrid,
-  createTab,
-  deleteByQuery,
-  deleteByRowId,
-  deleteTab,
-  deleteGrid,
-  getGridMetadata,
-  getMultisheetMetadata,
-  insert,
-  search,
-  searchCount,
-  searchDistinct,
-  setupLinkedColumn,
-  updateByQuery,
-  updateByRowId,
-  updateColumnDatatype,
-  updateColumnDataSource,
-  updateTab,
-};
-
+export { addColumn } from './addColumn';
+export { bulkCrud } from './bulkCrud';
+export { createGrid } from './createGrid';
+export { createTab } from './createTab';
+export { deleteByQuery } from './deleteByQuery';
+export { deleteByRowId } from './deleteByRowId';
+export { deleteTab } from './deleteTab';
+export { deleteGrid } from './deleteGrid';
+export { getGridMetadata } from './getGridMetadata';
+export { getMultisheetMetadata } from './getMultisheetMetadata';
+export { insert } from './insert';
+export { search } from './search';
+export { searchCount } from './searchCount';
+export { searchDistinct } from './searchDistinct';
+export { setupLinkedColumn } from './setupLinkedColumn';
+export { updateByQuery } from './updateByQuery';
+export { updateByRowId } from './updateByRowId';
+export { updateColumnDatatype } from './updateColumnDatatype';
+export { updateColumnDataSource } from './updateColumnDataSource';
+export { updateTab } from './updateTab';
 export * from './types';
